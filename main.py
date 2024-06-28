606193cb10eef5083deb4a4ee2441de58165c3fd
Tom <tomrg47@gmail.com>
2024-06-28 17:45:06 +0100

    In app title added

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from guizero import *
-app = App(title = "Image Sroter")
+
+app = App(title="Image Sorter")
 
 
+def ui():
+    #app.bg()
+    title_box = Box(app,width="fill",align="top")
+    title_text = Text(title_box, text="Image Sorter",font="arial",size=30)
+
+
+ui()
 app.display()
